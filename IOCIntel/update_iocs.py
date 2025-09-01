a1422fb371a65057b872ff1432c3ba2d0f5f467c
Abdul Uman <abduluman1@gmail.com>
2025-09-01 12:36:44 +0530

    Update update_iocs.py

--- IOCIntel/update_iocs.py
+++ IOCIntel/update_iocs.py
@@ -1,1 +1,13 @@
+IOC_DIR = "IOCIntel"
 
+files = {
+    "ip": f"{IOC_DIR}/ip_addresses.txt",
+    "c2": f"{IOC_DIR}/c2.txt",
+    "commands": f"{IOC_DIR}/commands.txt",
+    "domains": f"{IOC_DIR}/domains.txt",
+    "urls": f"{IOC_DIR}/urls.txt",
+    "hashes": f"{IOC_DIR}/hashes.txt",
+    "hostnames": f"{IOC_DIR}/hostnames.txt",
+    "cves": f"{IOC_DIR}/cves.txt",
+    "signers": f"{IOC_DIR}/signers.txt"
+}
